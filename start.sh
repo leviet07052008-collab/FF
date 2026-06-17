66aa89053784aaee05118d5a4fd59657609dd772
leviet07052008-collab <leviet07052008@gmail.com>
2026-06-17 11:06:21 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "=========================================="
+echo "  FREEFIRE PROXY HACK - STARTING"
+echo "=========================================="
+
+echo "Installing dependencies..."
+pip3 install -r requirements.txt
+
+echo "Starting system..."
+python3 main.py
